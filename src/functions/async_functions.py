258ffd32d8ce8b79a572f77a1d2586db8d9611b8
Kailas-Thonnithodi <161798595+Kailas-Thonnithodi@users.noreply.github.com>
2025-09-30 14:14:42 +1000

    Converted all functions and smart_agents into packages

---
+++ src/functions/async_functions.py
@@ -0,0 +1,9 @@
+# Libraries
+from agents import Agent, Runner, trace, function_tool
+from pydantic import BaseModel, Field
+from agents.model_settings import ModelSettings
+from dotenv import load_dotenv
+import smart_agents
+
+# print(agent_properties.GPT_MODELS["default"])
+
