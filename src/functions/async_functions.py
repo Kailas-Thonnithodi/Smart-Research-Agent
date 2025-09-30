3745d0bca151c2fc2b13f2d622714fde86d8c09c
Kailas-Thonnithodi <161798595+Kailas-Thonnithodi@users.noreply.github.com>
2025-09-30 16:51:37 +1000

    Updated async_functions.py so main functionality can operate on main.py file

--- src/functions/async_functions.py
+++ src/functions/async_functions.py
@@ -4,25 +4,25 @@ import asyncio
 from src.smart_agents import searcher, search_optimiser, writer, emailer
 
 async def plan_searches(query: str):
-    """ Use the planner_agent to plan which searches to run for the query """
+    """Calling search_optimiser to find an instance of prompt for the query"""
     print("Planning searches...")
     result = await Runner.run(search_optimiser.search_optimiser_agent, f"Query: {query}")
     print(f"Will perform {len(result.final_output.searches)} searches")
     return result.final_output
-
-async def perform_searches(search_plan: search_optimiser.WebSearchPlan):
-    """ Call search() for each item in the search plan """
-    print("Searching...")
-    tasks = [asyncio.create_task(search(item)) for item in search_plan.searches]
-    results = await asyncio.gather(*tasks)
-    print("Finished searching")
-    return results
 
 async def search(item: search_optimiser.WebSearchTerm):
     """ Use the search agent to run a web search for each item in the search plan """
     input = f"Search term: {item.query}\nReason for searching: {item.reason}"
     result = await Runner.run(searcher.searcher_agent, input)
     return result.final_output
+
+async def perform_searches(search_plan: search_optimiser.WebSearchPlan):
+    """ Calling search_optimiser to discover x amount of prompts based on the query provided."""
+    print("Searching...")
+    tasks = [asyncio.create_task(search(item)) for item in search_plan.searches]
+    results = await asyncio.gather(*tasks)
+    print("Finished searching")
+    return results
 
 async def write_report(query: str, search_results: list[str]):
     """ Use the writer agent to write a report based on the search results"""
@@ -38,15 +38,3 @@ async def send_email(report: writer.ReportData):
     result = await Runner.run(emailer.emailer_agent, report.markdown_report)
     print("Email sent")
     return report
-
-query ="Latest AI Agent frameworks in 2025"
-
-async def main(query: str):
-    with trace("Research trace"):
-        print("Starting research...")
-        search_plan = await plan_searches(query)
-        search_results = await perform_searches(search_plan)
-        report = await write_report(query, search_results)
-        await send_email(report)  
-
-asyncio.run(main("OpenAI innovations for 2025."))
---
+++ src/main.py
@@ -0,0 +1,15 @@
+from src.functions import async_functions
+from agents import trace
+import asyncio
+
+query = "OpenAI Technologies Upcoming in the next 5 years."
+
+async def main(query: str):
+    with trace("Research trace"):
+        print("Starting research...")
+        search_plan = await async_functions.plan_searches(query)
+        search_results = await async_functions.perform_searches(search_plan)
+        report = await async_functions.write_report(query, search_results)
+        await async_functions.send_email(report)  
+
+asyncio.run(main("OpenAI innovations for 2025."))
