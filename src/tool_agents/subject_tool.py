5aff3890af7bc70228c8b71ee8a8e51b4b036225
Kailas-Thonnithodi <161798595+Kailas-Thonnithodi@users.noreply.github.com>
2025-10-02 13:06:32 +1000

    Updated changes to the subject_tool.py

--- src/tool_agents/subject_tool.py
+++ src/tool_agents/subject_tool.py
@@ -1,4 +1,4 @@
-from agents import Agent, function_tool
+from agents import Agent
 
 instructions = "Could you write a subject for the email.\n" \
                "You are given the report, you need to write a subject for an email, in order to make the report appealing to click."
