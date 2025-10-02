52896cd71afa87c43d3c73e4f6247cbfba7c2a32
Kailas-Thonnithodi <161798595+Kailas-Thonnithodi@users.noreply.github.com>
2025-10-02 13:34:26 +1000

    Optimised instructions for emailer

--- src/tool_agents/html_formatter_tool.py
+++
@@ -1,8 +0,0 @@
-from agents import Agent
-
-instructions = "You can convert a text body to a HTML body.\n" \
-               "You care given a text body which have some markdown and you need to convert it into a HTML body with a simple,\
-                clear completing layout and desirable to read by a person viewing a report."
-
-html_formatter_agent = Agent(name="HTML Formatter", instructions=instructions, model="gpt-4o-mini")
-html_formatter_tool = html_formatter_agent.as_tool(name="HTML Formatter Tool", tool_description="Converts the body of text from plain to a nice html formatting report.")
